28b7261fd49304cc53dbfce9cfc9852b04301d43
kh.gil <kh.gil@spc.co.kr>
2025-08-27 16:10:17 +0900

    feat: api initial commit

---
+++ api/billiards-rank-api/src/main/java/com/myapp/billiards/BilliardsRankApiApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.billiards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardsRankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilliardsRankApiApplication.class, args);
+	}
+
+}
---
+++ api/billiards-rank-api/src/test/java/com/myapp/billiards/BilliardsRankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.billiards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardsRankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
